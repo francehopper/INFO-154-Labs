671ee18c936504d66b445d8ece04d928bd0a4b59
gmw45 <gmw45@drexel.edu>
2014-08-04 19:24:39 -0400

    The Database is working Data is not.
    The data is not flowing! Insert all the rage!

--- TwitterLab/searchForTweets.php
+++ TwitterLab/searchForTweets.php
@@ -36,6 +36,29 @@
       echo $outputTweet; // echo the tweet
   }
 
+$username = "root";
+$password = "info154";
+$hostname = "localhost:3306"; 
+
+//connection to the database
+$dbhandle = mysql_connect($hostname, $username, $password) 
+  or die("Unable to connect to MySQL");
+echo "Connected to MySQL<br>";
+
+//select a database to work with
+$selected = mysql_select_db("tweets",$dbhandle) 
+  or die("Could not select examples");
+
+  //function insertTweets($tweets){
+   
+
+
+//execute the SQL query and return records
+$result = mysql_query("SELECT username FROM tweets");
+//fetch tha data from the database
+while ($row = mysql_fetch_array($result)) {
+   echo "username:".$row{'username'};
+ }
 ?>
 </body>
 </html>
