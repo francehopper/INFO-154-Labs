94629fd674117a22f4720cfe1d676a9ecf4477b1
Stephen J. Weber <francehopper@gmail.com>
2014-08-05 19:52:49 -0400

    Bug fix
    Mmm, bugs.

--- DB Lab/history.php
+++ DB Lab/history.php
@@ -14,9 +14,9 @@ $theQuery = 'select * from tweets where tweet_text like \'%'.$searchString.'%\''
 $result = mysqli_query($connection, $theQuery) or die(mysqli_error($connection)); // perform query
 
 // render navigation
-echo '<a href=\"home.html\"><- Return home</a>';
-echo ' | ';
-echo '<a href=\"liveSearch.php?userString='.$searchString.'\">View the latest tweets about '.$searchString.'</a><br />';
+echo "<a href=\"home.html\"><- Return home</a>";
+echo " | ";
+echo "<a href=\"liveSearch.php?userString='.$searchString.'\">View the latest tweets about '.$searchString.'</a><br />";
 
 // show results
 echo '<strong><p>Historical tweets about '.$searchString.':</p></strong>'; // render header
